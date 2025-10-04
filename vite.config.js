a505d849681d4b3aee68b25c5b06770dd1a77dbc
Mohamed Amged <mohamedamged55555@gmail.com>
2025-10-04 17:50:30 +0300

    make update in vite.config

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/',
   plugins: [react()],
+  build: {
+    outDir: 'dist'
+  },
+  server: {
+    historyApiFallback: true
+  }
 })
